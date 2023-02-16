ed3ca7568c8d9e2bd808de39ba4373e26f6261f4
Barbaqa <zauribarbaqadze.main@gmail.com>
2023-02-16 22:12:26 +0400

    First JavaScript Project

---
+++ JavaScripts/main.js
@@ -0,0 +1,6 @@
+const loginform = document.querySelector('.auth');
+const loginbtn = document.querySelector('#loginbtn');
+
+loginbtn.addEventListener('click', () => {
+    loginform.style.display = 'flex';
+});
