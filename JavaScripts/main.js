bf3d80b0b36fa8923b3c4562f290f0923233ae1a
Barbaqa <zauribarbaqadze.main@gmail.com>
2023-02-17 20:51:59 +0400

    Auth/Reg Update

--- JavaScripts/main.js
+++ JavaScripts/main.js
@@ -1,6 +1,20 @@
 const loginform = document.querySelector('.auth');
 const loginbtn = document.querySelector('#loginbtn');
+const regbtn = document.querySelector('#regbtn');
+const regform = document.querySelector('.regstyle');
+const regbtn2 = document.querySelector('.reg-style');
 
 loginbtn.addEventListener('click', () => {
     loginform.style.display = 'flex';
+    regform.style.display = 'none';
+});
+
+regbtn.addEventListener('click', () => {
+    loginform.style.display = 'none';
+    regform.style.display = 'flex';
+});
+
+regbtn2.addEventListener('click', () => {
+    loginform.style.display = 'none';
+    regform.style.display = 'flex';
 });
