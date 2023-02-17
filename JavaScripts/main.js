95c4dcc5ef5bf895ed2a3a581824b4bb7dbb348d
Barbaqa <zauribarbaqadze.main@gmail.com>
2023-02-18 00:47:07 +0400

    JavaScript Changes

--- JavaScripts/main.js
+++ JavaScripts/main.js
@@ -1,6 +1,7 @@
 const loginform = document.querySelector('.auth');
 const loginbtn = document.querySelector('#loginbtn');
 const regbtn = document.querySelector('#regbtn');
+const logbtn = document.querySelector('.avttxt');
 const regform = document.querySelector('.regstyle');
 const regbtn2 = document.querySelector('.reg-style');
 
@@ -9,6 +10,10 @@ loginbtn.addEventListener('click', () => {
     regform.style.display = 'none';
 });
 
+logbtn.addEventListener('click', () => {
+    loginform.style.display = 'flex';
+    regform.style.display = 'none';
+});
 regbtn.addEventListener('click', () => {
     loginform.style.display = 'none';
     regform.style.display = 'flex';
